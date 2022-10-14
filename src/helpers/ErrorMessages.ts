4c34b8c52b374a4207d33923aeded7b9ea2b7723
roberio <roberio-almeida@hotmail.com>
2022-10-14 00:41:00 -0300

    chore: create enum helper errorMessages

---
+++ src/helpers/ErrorMessages.ts
@@ -0,0 +1,9 @@
+enum ErrorMessages {
+  InternalError = 'Error when trying to perform operation',
+  InvalidLogin = 'Incorrect email or password',
+  TokenNotFound = 'Token not found',
+  InvalidToken = 'Token must be a valid token',
+  RequiredField = 'All fields must be filled',
+}
+
+export default ErrorMessages;
